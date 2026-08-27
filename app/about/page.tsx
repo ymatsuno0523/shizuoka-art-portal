38cfca167d9f3038dcda5b1ef0001c13c9d8d784
Yuya Matsuno <y.matsuno0523@gmail.com>
2026-08-28 08:19:05 +0900

    Initial commit

---
+++ app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function AboutPage() {
+  return (
+    <main style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>About ページ</h1>
+      <p>ここはアバウトページです。</p>
+    </main>
+  );
+}
